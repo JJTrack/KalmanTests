450994a2afd05251ade540dff39c78af1efb9170
Jonathan <rabadunk@gmail.com>
2020-08-01 03:02:56 +1200

    made second graph

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Graph showing 5dBm rssi values at each distance against expected values
+
 let distances = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let rssiVals = [65.7, 77.2, 83.6, 83, 81.1, 83.8, 85.1, 88.8, 83.8];
 // 𝐴−10𝑛log(𝑑).
-let rssiFormula = rssiVals.map((rssi) => {
-    let A = 65.7;
+let rssiFormula = distances.map((distance) => {
+    let A = -65.7;
     let n = 2;
     
-    return A - 10*n*Math.log()
+    return (A - 10*n*Math.log(distance)) * -1
 })
-
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
@@ -21,6 +22,14 @@ var myChart = new Chart(ctx, {
         borderColor: "red",
         borderWidth: 1,
         responsive: true
+        },
+        {
+        label: 'Expected RSSI (-dBm)',
+        data: rssiFormula,
+        backgroundColor: "rgba(0, 0, 0, 0)",
+        borderColor: "blue",
+        borderWidth: 1,
+        responsive: true
         }]
     },
     options: {
@@ -32,6 +41,59 @@ var myChart = new Chart(ctx, {
                     suggestedMin: 50
                 }
             }]
+        },
+        title: {
+            display: true,
+            text: "Measured RSSI vs Expected RSSI at -5dBm"
         }
     }
 });
+
+// Graph showing 5dBm rssi values at 1m, measured vs filtered values
+
+let rssiMeasures = [71, 62, 61, 72, 67, 66, 64, 63, 65, 66];
+let kf = new KalmanFilter({R: 0.01, Q: 3});
+let filteredRssi = rssiMeasures.map((rssi)=>{
+    return kf.filter(rssi);
+})
+
+
+var con = document.getElementById('kalman').getContext('2d');
+var myChart = new Chart(con, {
+    type: 'line',
+    data: {
+        labels: distances,
+        datasets: [{
+            label: 'RSSI (-dBm)',
+            data: rssiMeasures,
+        backgroundColor: "rgba(0, 0, 0, 0)",
+        borderColor: "red",
+        borderWidth: 1,
+        responsive: true
+        },
+        {
+        label: 'filtered RSSI (-dBm)',
+        data: filteredRssi,
+        backgroundColor: "rgba(0, 0, 0, 0)",
+        borderColor: "blue",
+        borderWidth: 1,
+        responsive: true
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false,
+                    suggestedMax: 100,
+                    suggestedMin: 50
+                }
+            }]
+        },
+        title: {
+            display: true,
+            text: "Measured RSSI vs Expected RSSI at -5dBm"
+        }
+    }
+});
+
